da0e5e436ab4f5ec9c50711828647f4ba6a57038
putriagis <putribalqisnurhayati@gmail.com>
2026-03-09 00:03:20 +0700

    Menambahkan tampilan GUI kalkulator

--- TP_MODUL3_103022400081/Form1.cs
+++ TP_MODUL3_103022400081/Form1.cs
@@ -6,5 +6,15 @@ namespace TP_MODUL3_103022400081
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
